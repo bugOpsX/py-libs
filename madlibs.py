3d359ade79e0be5453d7fa7eecc006f5e8e0e654
Sumant Kumar <sumantkumar3172@gmail.com>
2025-07-02 00:05:37 +0530

    create your own story

---
+++ madlibs.py
@@ -0,0 +1,20 @@
+#word game where you create a story 
+
+
+print("Lets Make a Story By Your Choice!")
+print("Sounds Exiting Right!!")
+
+name = str(input("Enter Name of Character: "))
+place = input("Any place: ")
+pet = input("Any pet name: ")
+food = input("Any snacks: ")
+expression = input("Expression of Character: ")
+verb = input ("Any happeing task: ")
+
+print(f"My name is {name} a very dumb and lazy person.", 
+      f"But Surprisingly today I went to {place} where people like was never comes."
+      "Beside that, there was a park, and people took there pets for walk."
+      f"A person came near me and he has a {pet} with him."
+      f"I was sitting and eating {food}, that {pet} was looking at me as he wanted to snatch my World's Last Bite."
+      f"But as i told you I am lazy, I ignored him and {expression} at him."
+      f"After that the {pet} starts {verb}.")
